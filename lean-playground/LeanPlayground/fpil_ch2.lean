b78750ff8fc226bf39ea79bcb75025af81559c9d
Tony Zhang <nz97@cornell.edu>
2025-10-25 21:10:02 -0700

    chapter 2 proofs

--- lean-playground/LeanPlayground/fpil_ch2.lean
+++ lean-playground/LeanPlayground/fpil_ch2.lean
@@ -27,3 +27,52 @@ def main : IO Unit := do
   let englishGreeting := IO.println "Hello!"
   IO.println "Bonjour!"
   englishGreeting
+
+def woodlandCritters : List String :=
+  ["hedgehog", "deer", "snail"]
+
+def hedgehog := woodlandCritters[0]
+def deer := woodlandCritters[1]
+def snail := woodlandCritters[2]
+
+def OnePlusOneIsTwo : Prop := 1 + 1 = 2
+
+#check OnePlusOneIsTwo
+
+theorem onePlusOneIsTwo : 1 + 1 = 2 := by
+  decide
+
+theorem addAndAppend : 1 + 1 = 2 ∧ "Str".append "ing" = "String" := by
+  decide
+
+theorem andImpliesOr : A ∧ B → A ∨ B :=
+  fun andEvidence =>
+    match andEvidence with
+    | And.intro a b => Or.inl a
+
+def third (xs : List α) (ok : xs.length > 2) : α := xs[2]
+#eval third woodlandCritters (by decide)
+
+
+/- Proof Exercises -/
+
+theorem t1 : 2 + 3 = 5 := by
+  rfl
+
+theorem t2 : 15 - 8 = 7 := by
+  rfl
+
+theorem t3 : "Hello".append "World" = "HelloWorld" := by
+  rfl
+
+theorem t4 : 5 < 18 := by
+  omega
+
+
+def fifth (xs : List α) (ok : xs.length > 4) : α :=
+  xs[4]
+
+def woodlandCrittersPlus : List String :=
+  ["hedgehog", "deer", "snail", "fox", "rabbit"]
+
+#eval fifth woodlandCrittersPlus (by decide)
